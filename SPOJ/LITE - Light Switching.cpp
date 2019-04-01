2023c36d34f42f48656f021f20852d63fa5f59c9
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2019-04-01 18:47:51 +0100

    solved

---
+++ SPOJ/LITE - Light Switching.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+#define SIZE 1000001
+
+int on[4*SIZE];
+int toggle[4*SIZE];
+
+void propagate (int node, int L, int R)
+{
+    int mid = (L + R) / 2;
+    int lf = node * 2;
+    int rt = node * 2 + 1;
+
+    toggle[node] = 0;
+    toggle[lf] ^= 1;
+    toggle[rt] ^= 1;
+
+    on[lf] = mid - L + 1 - on[lf];
+    on[rt] = R - mid - on[rt];
+}
+
+void update (int node, int L, int R, int l, int r)
+{
+    if (l > r)
+            return;
+    if (L == l && R == r) {
+            on[node] = r - l + 1 - on[node];
+            toggle[node] ^= 1;
+            return;
+    }
+    int mid = (L + R) / 2;
+    int lf = node * 2;
+    int rt = node * 2 + 1;
+    update (lf, L, mid, l, min (r, mid));
+    update (rt, mid+1, R, max (l, mid+1), r);
+
+    if (toggle[node])
+            propagate (node, L, R);
+    on[node] = on[lf] + on[rt];
+}
+
+int query (int node, int L, int R, int l, int r)
+{
+    if (l > r)
+            return 0;
+    if (L == l && R == r)
+            return on[node];
+    if (toggle[node])
+            propagate (node, L, R);
+    int mid = (L + R) / 2;
+    return query (node*2, L, mid, l, min (r, mid))
+            + query (node*2+1, mid+1, R, max (l, mid+1), r);
+}
+
+int main()
+{
+    int n, m, c, x, y;
+
+    scanf ("%d %d", &n, &m);
+
+    while (m--) {
+            scanf ("%d %d %d", &c, &x, &y);
+            if (c == 0)
+                    update (1, 1, n, x, y);
+            else
+                    printf ("%d\n", query (1, 1, n, x, y));
+    }
+
+    return 0;
+}
