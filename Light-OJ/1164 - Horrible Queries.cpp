dc1e5398fb0503a9d5ead229bc232df5c8400f00
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2019-04-04 08:55:23 +0100

    solved

--- Light-OJ/1164 - Horrible Queries.cpp
+++ Light-OJ/1164 - Horrible Queries.cpp
@@ -1,75 +1,62 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
-
+ 
 typedef long long pll;
-
+ 
 #define SIZE 100001
-
+ 
 struct data {
-    pll sum, lazy;
+    pll sum;
+    pll prop;
     data () {}
-    data (int s, int v)
-            : sum(s), lazy(v) {}
+    data (int v, int x)
+            : sum(v), prop(x) {}
 } seg[4*SIZE];
-
-void propagate (int node, int L, int R)
-{
-    int mid = (L + R) / 2;
-    int lf = node * 2;
-    int rt = node * 2 + 1;
-
-    seg[lf].lazy += seg[node].lazy;
-    seg[rt].lazy += seg[node].lazy;
-    seg[lf].sum += seg[node].lazy * (mid - L + 1);
-    seg[rt].sum += seg[node].lazy * (R - mid);
-    seg[node].lazy = 0;
-}
-
-void update (int node, int L, int R, int l, int r, int new_val)
+ 
+void update (int node, int L, int R, int l, int r, int add)
 {
     if (l > r)
             return;
     if (L == l && R == r) {
-            seg[node].sum += (r - l + 1) * new_val;
-            seg[node].lazy += new_val;
+            seg[node].sum += add * (r - l + 1);
+            seg[node].prop += add;
             return;
     }
-    if (seg[node].lazy)
-            propagate (node, L, R);
     int mid = (L + R) / 2;
     int lf = node * 2;
     int rt = node * 2 + 1;
-    update (lf, L, mid, l, min (r, mid), new_val);
-    update (rt, mid+1, R, max (l, mid+1), r, new_val);
-    seg[node].sum = seg[lf].sum + seg[rt].sum;
+    update (lf, L, mid, l, min (r, mid), add);
+    update (rt, mid+1, R, max (l, mid+1), r, add);
+    seg[node].sum = seg[lf].sum + seg[rt].sum + seg[node].prop * (R - L + 1);
 }
-
-pll query (int node, int L, int R, int l, int r)
+ 
+pll query (int node, int L, int R, int l, int r, pll carry)
 {
     if (l > r)
             return 0;
     if (L == l && R == r)
-            return seg[node].sum;
-    if (seg[node].lazy)
-            propagate (node, L, R);
+            return seg[node].sum + carry * (r - l + 1);
     int mid = (L + R) / 2;
-    return query (node * 2, L, mid, l, min (r, mid))
-            + query (node * 2 + 1, mid+1, R, max (l, mid+1), r);
+    int lf = node * 2;
+    int rt = node * 2 + 1;
+    return
+            query (lf, L, mid, l, min (r, mid), carry + seg[node].prop)
+            + query (rt, mid+1, R, max (l, mid+1), r, carry + seg[node].prop);
 }
-
+ 
 int main (int argc, char **argv)
 {
     int t, n, c, m, x, y, v, cs=0;
-
+ 
     scanf("%d", &t);
-
+ 
     while (t--) {
             scanf("%d %d", &n, &c);
-
+ 
             for (int i=1; i<=4*n; ++i)
                     seg[i] = data (0, 0);
-
+ 
             printf("Case %d:\n", ++cs);
             while (c--) {
                     scanf("%d %d %d", &m, &x, &y);
@@ -79,9 +66,9 @@ int main (int argc, char **argv)
                             update (1, 1, n, x, y, v);
                     }
                     else
-                            printf("%lld\n", query (1, 1, n, x, y));
+                            printf("%lld\n", query (1, 1, n, x, y, 0));
             }
     }
-
+ 
     return 0;
 }
