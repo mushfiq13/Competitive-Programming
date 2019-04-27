0ba482f019a39cfbe8c6f8bf0091bebc459a03c1
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2019-04-27 04:08:30 +0100

    implementation

---
+++ Codeforces/1155A - Reverse a Substring.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;;
+
+int main()
+{
+    int n;
+    string s;
+
+    cin >> n >> s;
+
+    for (int i=1; s[i]; ++i)
+        if (s[i-1] > s[i]) {
+            puts("YES");
+            printf ("%d %d\n", i, i+1);
+            return 0;
+        }
+
+    puts("NO");
+    return 0;
+}
