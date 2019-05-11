ee54130b597da444b9ffe61c485ca3b82847d5f2
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2019-05-11 13:28:19 +0100

    linked list - array implementation

---
+++ UVa/12657 -   Boxes in a Line.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int Max = 1e5 + 10;
+typedef struct Data data;
+
+struct Data {
+    int lf, rt;
+};
+
+data node[Max];
+
+void link (int a, int b)
+{
+    node[a].rt = b;
+    node[b].lf = a;
+}
+
+void Swap (int x, int y)
+{
+    int xlf = node[x].lf, xrt = node[x].rt;
+    int ylf = node[y].lf, yrt = node[y].rt;
+    if (node[x].rt != y && node[x].lf != y) {
+        link (y, xrt);
+        link (ylf, x);
+        link (xlf, y);
+        link (x, yrt);
+    }
+    else if (node[y].rt != x) {
+        link (xlf, y);
+        link (x, yrt);
+        link(y, x);
+    }
+    else {
+        link (ylf, x);
+        link (y, xrt);
+        link(x, y);
+    }
+}
+
+void shift_left (int x, int y)
+{
+    if (node[x].rt == y) return;
+    link (node[x].lf, node[x].rt);
+    link (node[y].lf, x);
+    link (x, y);
+}
+
+void shift_right (int x, int y)
+{
+    if (node[y].rt == x) return;
+    link (node[x].lf, node[x].rt);
+    link (x, node[y].rt);
+    link (y, x);
+}
+
+int main()
+{
+    int n, m, cs=0;
+
+    while (~scanf("%d %d", &n, &m)) {
+        for (int i=0; i<=n+1; ++i) {
+                node[i].lf = i-1;
+                node[i].rt = i+1;
+        }
+
+        int rev = 0;
+        for (int i=1; i<=m; ++i) {
+                int c, x, y;
+                scanf("%d", &c);
+                if (c == 4)
+                        rev ^= 1;
+                else {
+                        scanf("%d %d", &x, &y);
+                        if (c == 1) {
+                            if (rev == 0) shift_left(x, y);
+                            else shift_right(x, y);
+                        }
+                        else if (c == 2) {
+                            if (rev == 0) shift_right(x, y);
+                            else shift_left(x, y);
+                        }
+                        else
+                            Swap(x, y);
+                }
+        }
+
+        long long sum = 0;
+        if (rev == 0)
+            for (int i=node[0].rt; i <= n; i = node[node[i].rt].rt)
+                sum += i;
+        else
+            for (int i=node[n+1].lf; i > 0; i = node[node[i].lf].lf)
+                sum += i;
+
+        printf("Case %d: %lld\n", ++cs, sum);
+    }
+
+    return 0;
+}
