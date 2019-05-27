d26b18f00bc8890084f01a9f771da4b628da4eb4
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2019-05-27 19:55:30 +0600

    implementation, sorting

--- Codeforces/375B - Maximum Submatrix 2.cpp
+++ Codeforces/375B - Maximum Submatrix 2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 const int MAX = 5001;
 
-string s[MAX];
+char s[MAX];
 vector<int> mat[MAX];
 
 int main()
@@ -12,16 +12,18 @@ int main()
     cin >> n >> m;
 
     for (int i=0; i<n; ++i) {
-        cin >> s[i];
         int Count[m+1] = {};
+        getchar();
         for (int j=0; j < m; ++j) {
+            s[j] = getchar();
             if (!j)
-                Count[j] = s[i][j] - '0';
+                Count[j] = s[j] - '0';
             else
-                Count[j] = s[i][j-1]=='1' && s[i][j]=='1' ? Count[j-1]+1 : s[i][j]-'0';
+                Count[j] = s[j-1]=='1' && s[j]=='1' ? Count[j-1]+1 : s[j]-'0';
             if (Count[j])
                 mat[j].push_back(Count[j]);
         }
+
     }
 
     int ans = 0;
