44af8b6f918fba039369cc3bdd9de7d0ee98d16f
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2019-08-09 00:06:59 +0600

    solved

---
+++ Codeforces/1196A - Three Piles of Candies.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int q;
+
+    cin >> q;
+    while (q--) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        cout << (a+b+c) / 2 << endl;
+    }
+}
