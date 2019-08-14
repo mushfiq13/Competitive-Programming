6af018d3769e5ccabaacf2fa0dae0e925331a9c1
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2019-08-14 13:19:30 +0600

    implementation

---
+++ Toph/Groot's Mission.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1000 + 10;
+
+int main() {
+    int t, n;
+
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        for (int i=1; i<=n; ++i) {
+            int x;
+            scanf("%d", &x);
+        }
+        printf("%d\n", n-1);
+    }
+
+    return 0;
+}
