e54ab297bcb2d8b934ebf9184d6b3d25b6465bd8
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2020-06-12 16:25:37 +0600

    Add files via upload

--- Light-OJ/1334 - Genes in DNA.cpp
+++ Light-OJ/1334 - Genes in DNA.cpp
@@ -1,7 +1,14 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> prefixFunction(string& s) {
+int solve(int cur, vector<int>& pi, vector<int>& cnt) {
+    if (cur == 0) return 0;
+    if (cnt[cur]) return cnt[cur];
+    return solve(pi[cur-1], pi, cnt) + 1;
+}
+
+vector<int> prefixFunction(string& s, int m, vector<int>& cnt) {
     int n = (int)s.length();
     vector<int> pi (n+5);
     int j = 0;
@@ -10,28 +17,9 @@ vector<int> prefixFunction(string& s) {
             j = pi[j-1];
         if (s[i] == s[j]) ++j;
         pi[i] = j;
+        if (i > m && !cnt[j]) cnt[j] = solve(j, pi, cnt);
     }
     return pi;
-}
-
-void Count(int n, int m, vector<int>& pi, vector<int>& pp) {
-    for (int i=n; i<m; ++i) {
-        int cur = pi[i];
-        stack<int> st;
-        while (cur) {
-            st.push(cur);
-            if (pp[cur]) break;
-            cur = pi[cur-1];
-        }
-        if (st.empty()) continue;
-        if (!pp[st.top()]) pp[st.top()] = 1;
-        int cnt = pp[st.top()];
-        st.pop();
-        while (!st.empty()) {
-            pp[st.top()] = ++cnt;
-            st.pop();
-        }
-    }
 }
 
 int main() {
@@ -42,33 +30,23 @@ int main() {
     for (int Case=1; Case<=T; ++Case) {
         cin >> t >> s;
 
+        int n = (int)s.length();
+        vector<int> cnt1 (n+1);
         string a = s + '#' + t;
-        vector<int> pi1 = prefixFunction(a);
+        vector<int> pi1 = prefixFunction(a, n, cnt1);
 
         reverse(s.begin(), s.end());
         reverse(t.begin(), t.end());
 
         string b = s + '#' + t;
-        vector<int> pi2 = prefixFunction(b);
+        vector<int> cnt2 (n+1);
+        vector<int> pi2 = prefixFunction(b, n, cnt2);
 
-        int n = (int)s.length();
-        int m = (int)b.length();
-
-        int i = n+1, j = m-1;
-        while (i < j) {
-            swap(pi2[i], pi2[j]);
-            ++i, --j;
-        }
-
-        vector<int> pp1 (n+5), pp2 (n+5);
-        Count(n+1, m, pi1, pp1);
-        Count(n+1, m, pi2, pp2);
-
-        long long ans = 0;
-        for (int i=n+1; i<m-1; ++i) {
-            int x = pp1[pi1[i]], y = pp2[pi2[i+1]];
+        long long ans = 0, m = (int)a.length();
+        for (int i=n+1, j = m-2; i<m-1; ++i, --j) {
+            int x = cnt1[pi1[i]], y = cnt2[pi2[j]];
             if (pi1[i] == n) --x;
-            if (pi2[i+1] == n) --y;
+            if (pi2[j] == n) --y;
             ans += x * y;
         }
 
