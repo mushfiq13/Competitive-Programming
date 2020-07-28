b12588fa2103a79d8006bee33c93720f309e85e5
mushfiq4513 <39198267+mushfiq4513@users.noreply.github.com>
2020-07-29 00:03:52 +0600

    Add files via upload

---
+++ UVa/11790 - Murcia's Skyline.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n;
+
+    scanf("%d", &t);
+    for (int Case=1; Case<=t; ++Case) {
+        scanf("%d", &n);
+
+        int h[n], w[n];
+        for (int i=0; i<n; ++i) scanf("%d", &h[i]);
+        for (int i=0; i<n; ++i) scanf("%d", &w[i]);
+
+        vector<int> dp (n, 1), dp2 (n, 1);
+        int sumInc[n] = {}, sumDec[n] = {};
+        int ansInc = 0, ansDec = 0;
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<i; ++j) {
+                if (h[j] < h[i]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                    sumInc[i] = max(sumInc[i], sumInc[j]);
+                }
+                if (h[j] > h[i]) {
+                    dp2[i] = max(dp2[i], dp2[j]+1);
+                    sumDec[i] = max(sumDec[i], sumDec[j]);
+                }
+            }
+            sumInc[i] += w[i];
+            sumDec[i] += w[i];
+            ansInc = max(ansInc, sumInc[i]);
+            ansDec = max(ansDec, sumDec[i]);
+
+        }
+
+        if (ansInc >= ansDec)
+            printf("Case %d. Increasing (%d). Decreasing (%d).\n", Case, ansInc, ansDec);
+        else
+            printf("Case %d. Decreasing (%d). Increasing (%d).\n", Case, ansDec, ansInc);
+    }
+
+    return 0;
+}
